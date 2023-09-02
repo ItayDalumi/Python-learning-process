fda775c8c005940dc0a9eba286b6fcb29e2c0ca8
Itay Dalumi <itaydalumi4@gmail.com>
2023-09-02 18:37:34 +0300

    some additions

---
+++ enumaration.py
@@ -0,0 +1,4 @@
+#enumerate is a function that gives the values and their index from the list
+animals=["dog","cat","lion"]
+for index, animal in enumerate(animals):
+    print(f"{index} -> {animal}")
---
+++ list comprehension.py
@@ -0,0 +1,5 @@
+
+numbers1 = [1,2,3,5,8,13] # just a regular list.
+
+numbers2 = [num*2 for num in numbers1] # <-- this is list comprehension.
+print(numbers2) # output: [2, 4, 6, 10, 16, 26]
